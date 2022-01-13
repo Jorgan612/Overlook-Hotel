fbacea98ea33daa80a519bca1d263666f253785b
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-01-13 11:26:02 -0700

    Add implementation code and tests for Customer class properties and method.

--- classes/Customer.js
+++ classes/Customer.js
@@ -1,7 +1,25 @@
 class Customer {
-  constructor() {
-
+  constructor(customersData, bookings) {
+    this.id = customersData.id;
+    this.name = customersData.name;
+    this.bookings = bookings;
+// bookings currently has ALL of the customers in test datas bookings
   }
+getFirstName() {
+  // do you need to figure out a way to match customer
+  // id's to determine correct customer name?
+  // customersData on line 13 is undefined.
+
+  // const firstName = customersData.find((customer) => {
+  //   if (customer.id === id) {
+  //     return customer;
+  //   }
+  // })
+  // return firstName.name.split(' ')[0]
+  const firstName = this.name.split(' ');
+  return firstName[0];
+}
+
 }
 
 export default Customer;
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,16 +1,48 @@
 import { expect } from 'chai';
-import Customer from './classes/Customer.js';
+import Customer from '../classes/Customer.js';
 import { customersData, bookingsData, roomsData } from './test-dataSets';
 
 describe('Customer', function() {
 
   let customer;
+  let customer2;
 
   beforeEach(() => {
-    customer = new Customer(customersData)
+    customer = new Customer(customersData[0], bookingsData);
+    customer2 = new Customer(customersData[1], bookingsData);
+
   })
 
   it('It should be a function', function() {
     expect(Customer).to.be.a('function');
   });
+
+  it('It should instantiate a new instance of Customer', function() {
+    expect(customer).to.be.an.instanceOf(Customer);
+  });
+
+  it('It should have an id', function() {
+    expect(customer.id).to.be.an('number');
+    expect(customer.id).to.equal(1);
+  });
+
+  it('It should have a name', function () {
+    expect(customer.name).to.be.a('string');
+    expect(customer.name).to.equal('Leatha Ullrich');
+  });
+
+  it('It should be able to take in a different name', function () {
+    expect(customer2.name).to.equal('Rocio Schuster');
+  });
+
+  it('It should be able to get the customers first name', function () {
+    customer.getFirstName(customer);
+    expect(customer.getFirstName()).to.equal('Leatha');
+  });
+
+  it('It should have a list of bookings', function () {
+    console.log(customer.bookings)
+    expect(customer.bookings).to.be.an('array');
+  });
+
 });
