fd2a7f036d1a62e55467e648df356a96674f89e1
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-01-15 13:06:59 -0700

    Add day.js to project, add findAvailableBookings function to scripts to log selected calendar date in console.

--- classes/Hotel.js
+++ classes/Hotel.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 class Hotel {
   constructor(roomsData, bookingsData, customersData) {
     this.rooms = roomsData;
--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,6 @@
 import './css/base.scss';
 import domUpdates from './domUpdates.js';
+import dayjs from 'dayjs';
 
 import { fetchRooms,
   fetchBookings,
@@ -12,10 +13,19 @@ import Hotel from '../classes/Hotel.js';
 // images
 import './images/overlook-carpet-top-down.png';
 
+
+
+// Query Selectors
+
+const bookAvailableRoomButton = document.querySelector('#bookAvailableRoomButton');
+
+
+
 // Global variables
 
 let customer;
-let hotel;
+let hotel
+let today = dayjs().format('YYYY-MM-DD');
 
 
 const fetchAll = () => {
@@ -24,6 +34,8 @@ const fetchAll = () => {
     customer = new Customer(data[2][0]);
     hotel = new Hotel(data[0], data[1], data[2][0]);
     getCustomerInformation()
+    document.getElementById('date').setAttribute('min', today);
+    document.getElementById('date').setAttribute('value', today);
   })
 }
 
@@ -36,4 +48,16 @@ const getCustomerInformation = () => {
   domUpdates.displayPersonalizedGreeting(customer);
 }
 
+const findAvailableBookings = () => {
+  // event.preventDefault();
+  let calendarInputDate = document.getElementById('date');
+  let selectedCalendarInputDate = dayjs(calendarInputDate.value).format('YYYY/MM/DD');
+  // ABOVE VARIABLE PASSED INTO FUNCTION TO CHECK AVAILABILITY?!
+  console.log('new Date ------', selectedCalendarInputDate)
+}
+
+
+// Event Listeners
+
+bookAvailableRoomButton.addEventListener('click', findAvailableBookings);
 window.addEventListener('load', fetchAll);
