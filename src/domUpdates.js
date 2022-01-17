a8b27b0e2dcf2eb09bedb9cd95dbf4382ddb523e
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-01-17 13:47:50 -0700

    Add tabindex=0 to applicable fields for screenreader.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -14,11 +14,12 @@ displayTotalAmountSpentOnBookings(bookingAmount) {
   totalSpentOnBookings.innerText = `Total Spent:  $${bookingAmount}`;
   },
 displayCustomerInformation(bookingInfo) {
+
   const customerBookingsContainer = document.querySelector('#displayCustomerBookings');
   customerBookingsContainer.innerHTML = "";
   bookingInfo.forEach((booking) => {
     const bookingCard = `
-    <article class="user-booking-card">
+    <article class="user-booking-card" tabindex=0>
       <p class="card-details">Date - ${booking.date}</p>
       <p class="card-details">Room - ${booking.roomNumber}</p>
     </article>
